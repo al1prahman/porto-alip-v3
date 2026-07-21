9b5c41887df56ab88fa91f46f3f29a19e51f30db
al1prahman <rahmanalifm21@gmail.com>
2026-07-21 18:22:24 +0700

    feat: add UserCursor component with framer-motion integration; update package.json and package-lock.json for new dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/app/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider"; // Import Provider
 
 export const metadata: Metadata = {
